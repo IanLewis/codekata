be200d12c29e906a8d1377d6bd901ed2c1f733fa
Ian Lewis <IanMLewis@gmail.com>
2014-08-04 09:04:42 +0900

    Added recursive binary search implementation.

--- kata02/main.go
+++ kata02/main.go
@@ -9,16 +9,18 @@ var letters = []int64{2, 3, 5, 5, 15, 16, 22, 23, 26, 27, 27, 30, 32, 33, 38, 38
 
 var NOT_FOUND = -1
 
-func bsearch(s []int64, key int64) int {
+func bsearch_functional(s []int64, key int64) int {
     min := 0
     max := len(s) - 1
 
     // Check base condition
     if (max < min) {
-        return NOT_FOUND;
+        return NOT_FOUND
     }
 
     for min <= max {
+        // Find the midpoint without adding min and max
+        // and possibly causing an overflow error.
         midpoint := min + ((max - min) / 2)
 
         if (s[midpoint] == key) {
@@ -32,14 +34,35 @@ func bsearch(s []int64, key int64) int {
         }
     }
 
-    return NOT_FOUND;
+    return NOT_FOUND
 }
 
+func bsearch_recursive(s []int64, key int64) int {
+    return _bsearch_recursive(s, key, 0, len(s) - 1)
+}
+
+func _bsearch_recursive(s []int64, key int64, min int, max int) int {
+    if (max < min) {
+        return NOT_FOUND
+    }
+
+    // Find the midpoint without adding min and max
+    // and possibly causing an overflow error.
+    midpoint := min + ((max -min) / 2)
+    if (s[midpoint] == key) {
+        return midpoint
+    } else if (key < s[midpoint]) {
+        return _bsearch_recursive(s, key, min, midpoint - 1)
+    } else {
+        return _bsearch_recursive(s, key, midpoint + 1, max)
+    }
+}
 
 func main() {
     search_key, _ := strconv.ParseInt(os.Args[1], 10, 32)
 
-    index := bsearch(letters, search_key)
+    // index := bsearch_functional(letters, search_key)
+    index := bsearch_recursive(letters, search_key)
     if (index == -1) {
         fmt.Printf("%d not found.\n", search_key)
     } else {
